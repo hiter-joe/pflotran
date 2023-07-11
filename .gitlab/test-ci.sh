2c1727a526250638d7bbf7de018dd5de8d6f782d
Glenn Hammond <glenn.hammond@pnnl.gov>
2023-07-11 08:48:44 -0700

    testing wget

---
+++ .gitlab/test-ci.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# these paths were set in the Docker pflotran/mpich4:ubuntu22 image
+MPICH_DIR=/scratch/mpich-4.1
+MPICH_INSTALL_DIR=$MPICH_DIR/install
+
+mkdir -p $PETSC_DIR && cd $PETSC_DIR
+wget https://gitlab.com/petsc/petsc/-/archive/main/petsc-main.tar.gz && tar -xzvf petsc-main.tar.gz
+ls
